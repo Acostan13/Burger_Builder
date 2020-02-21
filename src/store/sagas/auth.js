04f39f3a5d32e79382d544764648fb279547e0df
Acostan13 <acostan@fau.edu>
2020-02-21 11:20:31 -0500

    handling authentication with a saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,7 +1,6 @@
-// import { delay } from 'redux-saga'
 import { put, delay } from 'redux-saga/effects'
+import axios from 'axios'
 
-//import * as actionTypes from '../actions/actionTypes'
 import * as actions from '../actions/index'
 
 export function* logoutSaga(action) {
@@ -12,6 +11,32 @@ export function* logoutSaga(action) {
 }
 
 export function* checkAuthTimeoutSaga(action) {
-    yield delay(action.expirationTime)
+    yield delay(action.expirationTime * 1000)
     yield put(actions.logout())
+}
+
+export function* authUserSaga(action) {
+    yield put(actions.authStart())
+    const authData = {
+        email: action.email,
+        password: action.password,
+        returnSecureToken: true
+    }
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAeb2TkSniex87DeIAxVRjQmzdHGVSMJhs'
+    if (!action.isSignup) {
+        url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAeb2TkSniex87DeIAxVRjQmzdHGVSMJhs'
+    }
+    try {
+        const response = yield axios.post(url, authData)
+
+        const expirationDate = yield new Date(new Date().getTime() + response.data.expiresIn * 1000)
+        yield localStorage.setItem('token', response.data.idToken)
+        yield localStorage.setItem('expirationDate', expirationDate)
+        yield localStorage.setItem('userId', response.data.localId)
+        yield put(actions.authSuccess(response.data.idToken, response.data.localId))
+        yield put(actions.checkAuthTimeout(response.data.expiresIn))
+    } catch (error) {
+        yield put(actions.authFail(error.response.data.error))
+    }
+
 }
