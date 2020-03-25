66f33ce14cbe404e72e4c0c09a776d9e6fed54c9
Acostan13 <acostan@fau.edu>
2020-03-25 10:56:00 -0400

    redux & ui state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,8 +22,6 @@ class BurgerBuilder extends Component {
 
     // modern approach to declaring state
     state = {
-        totalPrice: 4,
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: null
@@ -47,7 +45,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el
             }, 0)
-        this.setState({purchasable: sum > 0})
+        return sum > 0
     }
 
     purchaseHandler = () => {
@@ -95,7 +93,7 @@ class BurgerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
                         price={this.props.price}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}/>
                 </Aux>
             )
