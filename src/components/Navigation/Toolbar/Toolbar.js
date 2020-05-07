d802dae2591261e292e307a99ec2b8f81a77a6b4
Acostan13 <acostan@fau.edu>
2020-05-07 10:08:11 -0400

    Implementing the Burger Logo

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from './Toolbar.module.css'
+import Logo from '../../Logo/Logo'
 
 const toobar = (props) => {
     return (
         <div>
             <header className={classes.Toolbar}>
                 <div>MENU</div>
-                <div>LOGO</div>
+                <Logo />
                 <nav>
                     <ul>
                         ...
