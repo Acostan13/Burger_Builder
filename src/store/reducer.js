3e5ccf8c2b6f5b524cfa36747b09808d2ef911ec
Acostan13 <acostan@fau.edu>
2020-05-21 10:18:28 -0400

    finishing the reducer for ingredients

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,36 @@
 import * as actionTypes from './actions'
 
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        beef: 0
+    },
     totalPrice: 4
 }
 
-const reducer = (state = initialState, actions) => {
-    
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients, // Creating deep clone of the state as we don't want to mutate the state
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1 // Gets the number of ingredients, adds one, and assigns this to the ingredient total 
+                }
+            }
+        case actionTypes.REMOVE_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                }
+            }
+        default:
+            return state
+    }
 }
 
 export default reducer
