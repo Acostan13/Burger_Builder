6876ce8ca5ad55f1d8bf00c14bbb3641658b2230
Acostan13 <acostan@fau.edu>
2020-05-23 10:49:04 -0400

    working on action orders

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -92,8 +92,7 @@ class ContactData extends Component {
                 validation: {}
             }
         },
-        formIsValid: false,
-        loading: false
+        formIsValid: false
     }
 
     //Ensures the user input is valid for the form submissions
@@ -180,7 +179,7 @@ class ContactData extends Component {
                 <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
             </form>
         );
-        if (this.state.loading) {
+        if (this.props.loading) {
             form = <Spinner />
         }
         return (
@@ -195,14 +194,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onBurgerOrder: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+        onBurgerOrder: (orderData) => dispatch(actions.purchaseBurger(orderData))
     }
 }
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
